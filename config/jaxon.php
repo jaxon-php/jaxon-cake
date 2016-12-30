8838de745b04fcfe6e48681d0b5fdb45dee32e4c
feuzeu <thierry.feuzeu@gmail.com>
2016-12-30 06:34:11 -0500

    Added default config file.

---
+++ config/jaxon.php
@@ -0,0 +1,43 @@
+<?php
+
+return array(
+    'app' => array(
+        'controllers' => array(
+            // 'directory' => '',
+            // 'namespace' => '',
+            // 'separator' => '', // '.' or '_'
+            // 'protected' => array(),
+        ),
+    ),
+    'lib' => array(
+        'core' => array(
+            'language' => 'en',
+            'encoding' => 'UTF-8',
+            'request' => array(
+                'uri' => 'jaxon',
+            ),
+            'prefix' => array(
+                'class' => '',
+            ),
+            'debug' => array(
+                'on' => false,
+                'verbose' => false,
+            ),
+            'error' => array(
+                'handle' => false,
+            ),
+        ),
+        'js' => array(
+            'lib' => array(
+                // 'uri' => '',
+            ),
+            'app' => array(
+                // 'uri' => '',
+                // 'dir' => '',
+                // 'extern' => true,
+                // 'minify' => true,
+                'options' => '',
+            ),
+        ),
+    ),
+);
