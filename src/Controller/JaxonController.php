41416d1f2fc566385dcb67c05abb875b35e6e921
feuzeu <thierry.feuzeu@gmail.com>
2017-09-21 18:24:40 -0400

    Added processing callbacks to the request processing controller.

--- src/Controller/JaxonController.php
+++ src/Controller/JaxonController.php
@@ -7,6 +7,11 @@ use App\Controller\AppController;
 
 class JaxonController extends AppController
 {
+    /**
+     * Load the Jaxon component.
+     *
+     * @return void
+     */
     public function initialize()
     {
         parent::initialize();
@@ -14,6 +19,51 @@ class JaxonController extends AppController
         $this->loadComponent('Jaxon/Cake.Jaxon');
     }
 
+    /**
+     * Callback for initializing a Jaxon class instance.
+     * 
+     * This function is called anytime a Jaxon class is instanciated.
+     *
+     * @param object            $instance               The Jaxon class instance
+     *
+     * @return void
+     */
+    public function initInstance($instance)
+    {
+    }
+
+    /**
+     * Callback before processing a Jaxon request.
+     *
+     * @param object            $instance               The Jaxon class instance to call
+     * @param string            $method                 The Jaxon class method to call
+     * @param boolean           $bEndRequest            Whether to end the request or not
+     *
+     * @return void
+     */
+    public function beforeRequest($instance, $method, &$bEndRequest)
+    {
+    }
+
+    /**
+     * Callback after processing a Jaxon request.
+     *
+     * @param object            $instance               The Jaxon class instance called
+     * @param string            $method                 The Jaxon class method called
+     *
+     * @return void
+     */
+    public function afterRequest($instance, $method)
+    {
+    }
+
+    /**
+     * Process a Jaxon request.
+     * 
+     * The HTTP response is automatically sent back to the browser
+     *
+     * @return void
+     */
     public function index()
     {
         if(substr(Configure::version(), 0, 3) != '3.0')
@@ -24,6 +74,17 @@ class JaxonController extends AppController
         {
             $this->layout = 'ajax';
         }
+
+        $this->Jaxon->onInit(function($instance){
+            $this->initInstance($instance);
+        });
+        $this->Jaxon->onBefore(function($instance, $method, &$bEndRequest){
+            $this->beforeRequest($instance, $method, $bEndRequest);
+        });
+        $this->Jaxon->onAfter(function($instance, $method){
+            $this->afterRequest($instance, $method);
+        });
+
         // Process Jaxon request
         if($this->Jaxon->canProcessRequest())
         {
